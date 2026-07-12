3b6f88fe9398780b93e16c8430d248638f24b7b1
paredesvicente237-ctrl <paredesvicente237@gmail.com>
2026-07-12 03:13:01 -0400

    Actualiza número de contacto

--- components/siteData.ts
+++ components/siteData.ts
@@ -2,13 +2,13 @@ export const companyInfo = {
   legalName: "N Proyectos Ltda",
   shortName: "N Proyectos",
   tagline: "Servicios y Proyectos en Aceros",
-  phoneDisplay: "+56 9 50107432",
-  phoneHref: "tel:+56950107432",
+  phoneDisplay: "+569 3075 1812",
+  phoneHref: "tel:+56930751812",
   email: "nproyectosltda@gmail.com",
   emailHref: "mailto:nproyectosltda@gmail.com",
   address: "Av. Yungay 743, La Granja",
   whatsappHref:
-    "https://wa.me/56950107432?text=Hola%2C%20quiero%20cotizar%20un%20proyecto%20con%20N%20Proyectos%20Ltda.",
+    "https://wa.me/56930751812?text=Hola%2C%20quiero%20cotizar%20un%20proyecto%20con%20N%20Proyectos%20Ltda.",
   mapsEmbed:
     "https://www.google.com/maps?q=Av.+Yungay+743,+La+Granja,+Chile&output=embed",
 };
